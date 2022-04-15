0c5ad791187665e366b9e6caf86230574618a3ad
Guillem <guillem.mart7@gmail.com>
2022-04-15 16:36:11 +0200

    iteration 1 done

--- js/index.js
+++ js/index.js
@@ -1,6 +1,13 @@
 // Iteration 1: Names and Input
-//
+hacker1 = "Jason";
+console.log(`The driver's name is ${hacker1}.`);
+hacker2 = "Tom";
+console.log(`The navigator's name is ${hacker2}.`)
 // Iteration 2: Conditionals
 
 
 // Iteration 3: Loops
+
+// Bonus 1: lorem ipsum counter
+
+// Bonus 2: phrase to check
